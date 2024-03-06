54c8f1491e3cbb0fbbe91fbcdae280df31bbb6a6
RHrakib1 <rakibhasanmd457@gmail.com>
2024-03-06 21:07:56 +0600

    add some comments

--- storage.js
+++ storage.js
@@ -1,7 +1,7 @@
 // localstorage dekhar jonno pothome inpaction krte hbe j kono web site tar por storage a local storage a giye j kono akta link a click kore ja ja key value use kora hoise ta sob dekhabe okhane value key add kora jai "esc" btn chap dile amra console korar jonno kisu pai tar por "localstorage likhle ja ja use hoise web site a segula sob dekha jabe "
 
 
-// localstorage a key holo akta string value holo akta string 
+// localstorage a key holo akta string value holo akta string
 // kono datar key dile setar valu paite gele aita use krte hoi
 // localStorage.getItem('name')
 
@@ -14,9 +14,28 @@
 
 
 // example
-// kono key,value delete korar jonnno aita use kora hoi 
+// kono key,value delete korar jonnno aita use kora hoi
 // localStorage.removeItem("roll")
 
 // example
-// localsotrage clrar korar jonno 
+// localsotrage clrar korar jonno
 // localstorage.clear()
+
+
+
+// example
+// kono obj a string a rupantorito krte "JSON.stringify(persone)" use kora hoi r sei string theke obj paite hole "JSON.parse(personeString)" use kora hoi tahole obj er moto paoya jai
+
+/**
+ * JSON.stringify(persone)
+'{"name":"lal miya","age":"52","liveing":"gainbandgha"}'
+
+const personeString = JSON.stringify(persone)
+undefined
+
+personeString
+'{"name":"lal miya","age":"52","liveing":"gainbandgha"}'
+
+JSON.parse(personeString)
+{name: 'lal miya', age: '52', liveing: 'gainbandgha'}
+ */
