cbb8fcb2ecc7fe2eb6d6ff82e7107e9f5123a0b9
RHrakib1 <rakibhasanmd457@gmail.com>
2024-03-06 10:49:41 +0600

    first commit

---
+++ storage.js
@@ -0,0 +1,22 @@
+// localstorage dekhar jonno pothome inpaction krte hbe j kono web site tar por storage a local storage a giye j kono akta link a click kore ja ja key value use kora hoise ta sob dekhabe okhane value key add kora jai "esc" btn chap dile amra console korar jonno kisu pai tar por "localstorage likhle ja ja use hoise web site a segula sob dekha jabe "
+
+
+// localstorage a key holo akta string value holo akta string 
+// kono datar key dile setar valu paite gele aita use krte hoi
+// localStorage.getItem('name')
+
+
+
+// example
+// kono key o value set krte hole aita use krte hoi
+// localStorage.setItem("roll", "511100")
+
+
+
+// example
+// kono key,value delete korar jonnno aita use kora hoi 
+// localStorage.removeItem("roll")
+
+// example
+// localsotrage clrar korar jonno 
+// localstorage.clear()
